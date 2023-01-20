54e2c80cb282ec86a28c6a227e6b0bc45db7cf19
Suman Poudel <sumanpoudelsharma@gmail.com>
2023-01-20 17:57:19 +0545

    connecting pages

---
+++ js/home.js
@@ -0,0 +1,15 @@
+function home(){
+    window. location. href = "../html/home.html";
+}
+function team(){
+    window. location. href = "../html/team_list.html";
+}
+function result(){
+    window. location. href = "../html/fixture.html";
+}
+function about_us(){
+    window. location. href = "../html/about_us.html";
+}
+function gallery(){
+    window. location. href = "../html/gallery.html";
+}
